8ba220d971337b37f0507d0780dd24cff434345b
William Cheausky <williamchenausky@yahoo.com>
2021-03-30 11:31:42 -0400

    connect handlebars

--- server.js
+++ server.js
@@ -1,11 +1,26 @@
 const express = require('express');
+const expb = require('express-handlebars');
 // import sequelize connection
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.engine('handlebars', expbs());
+app.set('view engine', 'handlebars');
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+//routing
+app.get('/', (req, res) => {
+  res.render('homepage.handlebars')
+});
+app.get('/', (req, res) => {
+  res.render('cards.handlebars')
+});
+app.get('/', (req, res) => {
+  res.render('login.handlebars')
+});
+
 
 require('./routes/cardAPI.js')(app);
 require('./routes/gameAPI.js')(app);
