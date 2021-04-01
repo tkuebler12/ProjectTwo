030456aae64e59038b55932511f8b993c1f0ed6f
lmarrero8566 <reefloco@gmail.com>
2021-04-01 15:26:52 -0400

    new card API routes

--- routes/cardAPI.js
+++ routes/cardAPI.js
@@ -1,21 +1,44 @@
-//using pokemon sdk
-const pokemon = require('pokemontcgsdk');
+// //using pokemon sdk
+// const pokemon = require('pokemontcgsdk');
 
-pokemon.configure({apiKey: '12e417e968-a16c-4330-bba3-93de7507eeb1'});
+// pokemon.configure({apiKey: '12e417e968-a16c-4330-bba3-93de7507eeb1'});
 
 
-module.exports = (app) => {
- /*  app.get('/api/character/:id', (req, res) => {
-        pokemon.card.find(req.params.id)
-        .then(card => {
-            res.json(card) // "Charizard"
-        })
-    })*/
-    
-    app.get('/api/sets/:q', (req, res) => {
-        pokemon.set.all(req.params.q)
-    .then((cards) => {
-        res.json(cards) // "Base"
-    })
-})
+const router = require('express').Router();
+const { card } = require('../../models');
+
+router.post('/', async (req, res) => {
+  try {
+    const newCard = await card.create({
+      ...req.body,
+      user_id: req.session.user_id,
+    });
+
+    res.status(200).json(newCard);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
+router.delete('/:id', async (req, res) => {
+  try {
+    const cardData = await card.destroy({
+      where: {
+        id: req.params.id,
+        user_id: req.session.user_id,
+      },
+    });
+
+    if (!cardData) {
+      res.status(404).json({ message: 'No card found with this id!' });
+      return;
     }
+
+    res.status(200).json(cardData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
+
